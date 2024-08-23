a7c0ff0ccff2e93b670e97368f6dfc755146424d
Mubashir1920 <131754637+Mubashir1920@users.noreply.github.com>
2024-08-23 20:09:03 +0500

    cache Removed

--- utils/request.js
+++ utils/request.js
@@ -2,46 +2,42 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 
 // Get All Properties
-const fetchProperties = async ({showfeatured= false}={}) => {
-    try {
-        if(!apiDomain){
-            return []
-        }
-      const res = await fetch(`${apiDomain}/properties${showfeatured ?'/featured':''}`,{
-        cache:'no-store'
-      });
-  
-      if (!res.ok) {
-        throw new Error('Failed To Fetch Data');
-      }
-  
-      return res.json();
-    } catch (error) {
-      console.error('Error fetching properties:', error);
-      return []; // Return an empty array in case of error
+const fetchProperties = async ({ showfeatured = false } = {}) => {
+  try {
+    if (!apiDomain) {
+      return []
     }
-  };
+    const res = await fetch(`${apiDomain}/properties${showfeatured ? '/featured' : ''}`);
+
+    if (!res.ok) {
+      throw new Error('Failed To Fetch Data');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error('Error fetching properties:', error);
+    return []; // Return an empty array in case of error
+  }
+};
 
 
 // Get Single Property 
 const fetchProperty = async (id) => {
-    try {
-        if(!apiDomain){
-            return null
-        }
-      const res = await fetch(`${apiDomain}/properties/${id}`,{
-        cache:'no-store'
-      });
-  
-      if (!res.ok) {
-        throw new Error('Failed To Fetch Data');
-      }
-  
-      return res.json();
-    } catch (error) {
-      console.error('Error fetching properties:', error);
-      return null; // Return an empty array in case of error
+  try {
+    if (!apiDomain) {
+      return null
     }
-  };
+    const res = await fetch(`${apiDomain}/properties/${id}`);
 
-  export {fetchProperties,fetchProperty }
+    if (!res.ok) {
+      throw new Error('Failed To Fetch Data');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error('Error fetching properties:', error);
+    return null; // Return an empty array in case of error
+  }
+};
+
+export { fetchProperties, fetchProperty }
